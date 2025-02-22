5e9995e13b0beb91b797d16ee40a82bf9870dbcd
hector496 <you@example.com>
2025-02-21 21:22:23 -0800

    contact

---
+++ scripts/script1.js
@@ -0,0 +1,31 @@
+// keyword variableName = value;
+// var, let, const
+
+//String
+var firstName = "Hector";
+var lastName = "Sanchez";
+
+//concatenation
+console.log("Name: "+firstName + " " + lastName);
+
+firstName = "Fernanda";// cambia
+
+//numerical
+let myAge=99;
+let myHeight=100;
+
+console.log(myAge,myHeight);
+
+//boolean (true/false)
+let isStudent = true; 
+
+//math operations + - / *
+let num1=10;
+let num2=12;
+
+document.write("<p>El resultado es = " + (num1+num2) + "</p>");
+document.write("<p>El resultado es = " + (num1-num2) + "</p>" );
+document.write("<p>El resultado es = " + (num1*num2)) + "</p>";
+document.write("<p>El resultado es = " + (num1/num2).toFixed(3) + "</p>");
+
+//alert("Esto es un mensaje de alerta");
